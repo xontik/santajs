fe3c60f46b47d1cf442d6dac8eef0c968c644ec7
Benjamin Borlet <xontik@gmail.com>
2019-01-25 20:57:26 +0100

    Add Authentification system and some entities

--- prisma/index.js
+++ prisma/index.js
@@ -1,68 +1,20 @@
 const { prisma } = require('./generated/prisma-client')
 const { GraphQLServer } = require('graphql-yoga')
-
+const Query = require('./resolver/Query')
+const Mutation = require('./resolver/Mutation')
 const resolvers = {
-    Query: {
-        publishedPosts(root, args, context) {
-            return context.prisma.posts({ where: { published: true } })
-        },
-        post(root, args, context) {
-            return context.prisma.post({ id: args.postId })
-        },
-        postsByUser(root, args, context) {
-            return context.prisma.user({
-                id: args.userId
-            }).posts()
-        }
-    },
-    Mutation: {
-        createDraft(root, args, context) {
-            return context.prisma.createPost(
-                {
-                    title: args.title,
-                    author: {
-                        connect: { id: args.userId }
-                    }
-                },
-
-            )
-        },
-        publish(root, args, context) {
-            return context.prisma.updatePost(
-                {
-                    where: { id: args.postId },
-                    data: { published: true },
-                },
-
-            )
-        },
-        createUser(root, args, context) {
-            return context.prisma.createUser(
-                { name: args.name },
-            )
-        }
-    },
-    User: {
-        posts(root, args, context) {
-            return context.prisma.user({
-                id: root.id
-            }).posts()
-        }
-    },
-    Post: {
-        author(root, args, context) {
-            return context.prisma.post({
-                id: root.id
-            }).author()
-        }
-    }
+    Query,
+    Mutation
 }
 
 const server = new GraphQLServer({
     typeDefs: './schema.graphql',
     resolvers,
-    context: {
-        prisma
+    context: request => {
+        return {
+            ...request,
+            prisma,
+        }
     },
 })
 server.start({playground: '/playground'},() => console.log('Server is running on http://api.santajs.test'))
